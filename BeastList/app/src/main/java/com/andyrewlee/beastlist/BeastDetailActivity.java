fa0e6b31a14ceecd869833ea67013d54bba679d7
Andrew Lee <andyrewlee@gmail.com>
2015-11-16 16:13:02 -0800

    Abstract single fragment to abstract class

--- BeastList/app/src/main/java/com/andyrewlee/beastlist/BeastDetailActivity.java
+++ BeastList/app/src/main/java/com/andyrewlee/beastlist/BeastDetailActivity.java
@@ -1,23 +1,13 @@
 package com.andyrewlee.beastlist;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
-public class BeastDetailActivity extends FragmentActivity {
-
+public class BeastDetailActivity extends SingleFragmentActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-        FragmentManager fm = getFragmentManager();
-
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-        if(fragment == null) {
-            fragment = new BeastDetailFragment();
-            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
-        }
+    protected Fragment createFragment() {
+        return new BeastDetailFragment();
     }
 }
--- BeastList/app/src/main/java/com/andyrewlee/beastlist/BeastDetailFragment.java
+++ BeastList/app/src/main/java/com/andyrewlee/beastlist/BeastDetailFragment.java
@@ -1,11 +1,10 @@
 package com.andyrewlee.beastlist;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
